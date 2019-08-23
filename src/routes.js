4a7fa7f2b987cdb21ea13764b527f10ea797caef
anas <anas_zay@outlook.fr>
2019-08-23 10:50:32 +0100

    clean project+sequelizeworked

--- src/routes.js
+++ src/routes.js
@@ -1,43 +1,21 @@
 import React from 'react';
 
-const Breadcrumbs = React.lazy(() => import('./views/Base/Breadcrumbs'));
-const NewProject = React.lazy(() => import('./views/Base/Breadcrumbs/NewProject'));
+const touslesprojets = React.lazy(() => import('./views/touslesprojets'));
+const NewProject = React.lazy(() => import('./views/NewProject'));
+const ProjectSelecitonnes = React.lazy(() => import('./views/ProjetSelectionnes.js'));
 
-const Cards = React.lazy(() => import('./views/Base/Cards'));
-const Mademande = React.lazy(() => import('./views/Base/Mademande/Mademande'));
+const ProjetsenCours = React.lazy(() => import('./views/ProjetsenCours'));
+const Mademande = React.lazy(() => import('./views/Mademande'));
 
-const Carousels = React.lazy(() => import('./views/Base/Carousels'));
-const Collapses = React.lazy(() => import('./views/Base/Collapses'));
-const Dropdowns = React.lazy(() => import('./views/Base/Dropdowns'));
-const Forms = React.lazy(() => import('./views/Base/Forms'));
-const ListGroups = React.lazy(() => import('./views/Base/ListGroups'));
-const Navbars = React.lazy(() => import('./views/Base/Navbars'));
-const Navs = React.lazy(() => import('./views/Base/Navs'));
-const Paginations = React.lazy(() => import('./views/Base/Paginations'));
-const Popovers = React.lazy(() => import('./views/Base/Popovers'));
-const ProgressBar = React.lazy(() => import('./views/Base/ProgressBar'));
-const Switches = React.lazy(() => import('./views/Base/Switches'));
-const Tables = React.lazy(() => import('./views/Base/Tables'));
-const Tabs = React.lazy(() => import('./views/Base/Tabs'));
-const Tooltips = React.lazy(() => import('./views/Base/Tooltips'));
-const BrandButtons = React.lazy(() => import('./views/Buttons/BrandButtons'));
-const ButtonDropdowns = React.lazy(() => import('./views/Buttons/ButtonDropdowns'));
-const ButtonGroups = React.lazy(() => import('./views/Buttons/ButtonGroups'));
-const Buttons = React.lazy(() => import('./views/Buttons/Buttons'));
-const Charts = React.lazy(() => import('./views/Charts'));
+const Projetslibres = React.lazy(() => import('./views/Projetslibres'));
+
+const EnAttentes = React.lazy(() => import('./views/EnAttentes'));
+const Tous = React.lazy(() => import('./views/Tous'));
+const approuvees = React.lazy(() => import('./views/approuvees'));
 const Dashboard = React.lazy(() => import('./views/Dashboard'));
 const Dashboard1 = React.lazy(() => import('./views/Dashboard/Dashboard1'));
 
-const CoreUIIcons = React.lazy(() => import('./views/Icons/CoreUIIcons'));
-const Flags = React.lazy(() => import('./views/Icons/Flags'));
-const FontAwesome = React.lazy(() => import('./views/Icons/FontAwesome'));
-const SimpleLineIcons = React.lazy(() => import('./views/Icons/SimpleLineIcons'));
-const Alerts = React.lazy(() => import('./views/Notifications/Alerts'));
-const Badges = React.lazy(() => import('./views/Notifications/Badges'));
-const Modals = React.lazy(() => import('./views/Notifications/Modals'));
-const Colors = React.lazy(() => import('./views/Theme/Colors'));
-const Typography = React.lazy(() => import('./views/Theme/Typography'));
-const Widgets = React.lazy(() => import('./views/Widgets/Widgets'));
+const ProjetsSelectionnes = React.lazy(() => import('./views/ProjetsSelectionnes'));
 const Users = React.lazy(() => import('./views/Users/Users'));
 const User = React.lazy(() => import('./views/Users/User'));
 
@@ -47,47 +25,21 @@ const routes = [
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
   { path: '/dashboard1', name: 'Dashboard', component: Dashboard1 },
     { path: '/base/Mademande', name: 'Mademande', component: Mademande },
+  { path: '/base', exact: true, name: 'Base', component: ProjetsenCours },
+  { path: '/Liste/ProjetsenCours', name: 'Projetencours', component: ProjetsenCours },
+  { path: '/Liste/touslesprojets', name: 'projets', component: touslesprojets },
+  { path: '/Liste/Projetslibres', name: 'Projetslibres', component: Projetslibres },
+  { path: '/Etudiants/Tous', name: 'Etudiants', component: Tous },
+  { path: '/Etudiants/approuvées', name: 'Etudiants approuvées', component:approuvees },
+  { path: '/Etudiants/EnAttentes', name: 'EtudiantsEnAttente', component: EnAttentes },
 
-
-  { path: '/theme', exact: true, name: 'Theme', component: Colors },
-  { path: '/theme/colors', name: 'Colors', component: Colors },
-  { path: '/theme/typography', name: 'Typography', component: Typography },
-  { path: '/base', exact: true, name: 'Base', component: Cards },
-  { path: '/Liste/ProjetsenCours', name: 'Projetencours', component: Cards },
-  { path: '/base/forms', name: 'Forms', component: Forms },
-  { path: '/base/switches', name: 'Switches', component: Switches },
-  { path: '/base/tables', name: 'Tables', component: Tables },
-  { path: '/base/tabs', name: 'Tabs', component: Tabs },
-  { path: '/Liste/touslesprojets', name: 'projets', component: Breadcrumbs },
-  { path: '/Liste/Projetslibres', name: 'Projetslibres', component: Carousels },
-  { path: '/base/collapses', name: 'Collapse', component: Collapses },
-  { path: '/base/dropdowns', name: 'Dropdowns', component: Dropdowns },
-  { path: '/base/list-groups', name: 'List Groups', component: ListGroups },
-  { path: '/base/navbars', name: 'Navbars', component: Navbars },
-  { path: '/base/navs', name: 'Navs', component: Navs },
-  { path: '/base/paginations', name: 'Paginations', component: Paginations },
-  { path: '/base/popovers', name: 'Popovers', component: Popovers },
-  { path: '/base/progress-bar', name: 'Progress Bar', component: ProgressBar },
-  { path: '/base/tooltips', name: 'Tooltips', component: Tooltips },
-  { path: '/buttons', exact: true, name: 'Buttons', component: Buttons },
-  { path: '/buttons/buttons', name: 'Buttons', component: Buttons },
-  { path: '/Etudiants/Tous', name: 'Etudiants', component: ButtonDropdowns },
-  { path: '/Etudiants/approuvées', name: 'Etudiants approuvées', component: ButtonGroups },
-  { path: '/Etudiants/EnAttentes', name: 'EtudiantsEnAttente', component: BrandButtons },
-  { path: '/icons', exact: true, name: 'Icons', component: CoreUIIcons },
-  { path: '/icons/coreui-icons', name: 'CoreUI Icons', component: CoreUIIcons },
-  { path: '/icons/flags', name: 'Flags', component: Flags },
-  { path: '/icons/font-awesome', name: 'Font Awesome', component: FontAwesome },
-  { path: '/icons/simple-line-icons', name: 'Simple Line Icons', component: SimpleLineIcons },
-  { path: '/MesProjets', exact: true, name: 'Mes Projets', component: Alerts },
-  { path: '/MesProjets/ProjetsSelectionnes', name: 'Projet ProjetsSelectionnes', component: Breadcrumbs },
-    { path: '/MesProjets/ProjetsSelectionnes', name: 'Projet ProjetsSelectionnes ', component: Alerts },
+  { path: '/MesProjets', exact: true, name: 'Mes Projets', component: ProjetsSelectionnes },
+  { path: '/MesProjets/ProjetsSelectionnes', name: 'Projet ProjetsSelectionnes', component: ProjectSelecitonnes },
+    { path: '/MesProjets/ProjetsSelectionnes', name: 'Projet ProjetsSelectionnes ', component: ProjetsSelectionnes },
   { path: '/MesProjets/NewProject', name: 'NewProjet', component: NewProject },
 
-  { path: '/MesProjets/MesProjets', name: 'Mes Projets', component: BrandButtons },
-  { path: '/notifications/modals', name: 'Modals', component: Modals },
-  { path: '/widgets', name: 'Widgets', component: Widgets },
-  { path: '/charts', name: 'Charts', component: Charts },
+  { path: '/MesProjets/MesProjets', name: 'Mes Projets', component: EnAttentes },
+  
   { path: '/users', exact: true,  name: 'Users', component: Users },
   { path: '/users/:id', exact: true, name: 'User Details', component: User },
 ];
